19ee7fa0cd1110b3ee2205f0fd2586d8d4d807ff
igoryy <igor.siviero1@gmail.com>
2020-11-15 19:20:56 -0300

    XJ

---
+++ js/util.js
@@ -0,0 +1,18 @@
+function dataAtualFormatada(){
+    var data = new Date(),
+        dia  = data.getDate().toString(),
+        diaF = (dia.length == 1) ? '0'+dia : dia,
+        mes  = (data.getMonth()+1).toString(), //+1 pois no getMonth Janeiro começa com zero.
+        mesF = (mes.length == 1) ? '0'+mes : mes,
+        anoF = data.getFullYear();
+    return diaF+"/"+mesF+"/"+anoF;
+}
+
+function scrollJob() {
+    //window.scrollTo(0, 450);
+    document.querySelector('.meio').scrollIntoView(
+        {
+            behavior: 'smooth'
+        }
+    );
+}
